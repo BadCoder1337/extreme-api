93da7e9e564ca691e58883ca0e8bd7418c0c3a04
AD-158 <alekseydenisov01@mail.ru>
2024-11-10 14:09:09 +0300

    feat: add db_init submodule.

---
+++ backend/authorization_service/app/utils/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __package__ is None or __package__ == '':
+    import sys
+    from os import path
+
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+    from backend.authorization_service.app.config import config
+else:
+    from ..config import config
+
+# SQLAlchemy database URL
+DATABASE_URL = config.DATABASE_URL
+
+# Create the SQLAlchemy engine
+engine = create_engine(url=DATABASE_URL, echo=True, echo_pool="debug")
+
+# SessionLocal is a factory for creating new sessions with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Dependency function
+def get_db():
+    db = SessionLocal()  # Create a new session
+    try:
+        yield db  # Provide the session to the request
+    finally:
+        db.close()  # Ensure session is closed after request completion
