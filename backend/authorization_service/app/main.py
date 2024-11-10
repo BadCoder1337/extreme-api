321dead546c1a22549cedf8ca23678eef869eb25
AD-158 <alekseydenisov01@mail.ru>
2024-11-10 19:30:44 +0300

    doc: add docstrings.

--- backend/authorization_service/app/main.py
+++ backend/authorization_service/app/main.py
@@ -19,13 +19,27 @@ app = FastAPI()
 
 @app.post("/logout")
 async def logout(request: Request):
+    """
+    Endpoint for logging out.
+
+    Args:
+        request: request object
+
+    Returns:
+        Ok, if successful, and error, if not.
+    """
     try:
         blacklist_token(request.cookies.get('refresh_token', None))
         return Response(content={"message": "Successfully logged out."}, status_code=status.HTTP_200_OK)
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-# docker-compose healthcheck address
 @app.get("/_health")
 def health_check():
-    return {"status": "healthy"}
+    """
+    Endpoint for docker-compose healthcheck call.
+
+    Returns:
+        Ok, if server is up and healthy.
+    """
+    return Response(content={"message": "Healthy"}, status_code=status.HTTP_200_OK)
